2d0b411cfcece231b87e6bfe22f6ded521da85ef
misconceptor <thespeedcubers347@gmail.com>
2026-04-16 17:47:38 -0400

    arena-0

---
+++ include/arena.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <exception>
+#include <algorithm>
+#include <cstdlib>
+//delete copy ctor and copy =
+class arena{
+private:
+    char *begin, *offset, *end;
+    bool owns_memory = false;
+public:
+    size_t size() const {
+        return end - begin;
+    }
+    size_t used() const {
+        return offset - begin;
+    }
+    char* align(char* _offset, size_t alignment){
+        size_t addr = (size_t)_offset;
+        addr = (addr + alignment - 1) & ~(alignment - 1);
+        _offset = (char*)addr;
+        return _offset;
+    }
+    arena(size_t size){
+        void* buf = malloc(size);
+        if (buf) {
+            std::cout << "allocation succeeded, size: " << size << '\n';
+            owns_memory = true;
+            offset = begin = (char*)buf; 
+            end = begin + size;
+        } else {
+            std::cout << "allocation failed\n";
+            owns_memory = false;
+            begin = end = offset = nullptr;
+            throw std::runtime_error("buffer is null");
+        }
+    }
+    arena(void* buf, size_t size){ //does not own memory, so can't free it in dtor
+        owns_memory = false;
+        if (buf) {
+            std::cout << "allocation succeeded, size: " << size << std::endl;
+            offset = begin = (char*) buf;
+            end = begin + size;
+        } else {
+            std::cout << "allocation failed" << std::endl;
+            begin = end = offset = nullptr;
+            throw std::invalid_argument("null buffer");
+        }
+    }
+    ~arena(){
+        if(owns_memory && begin) {
+            std::cout << "dtor" << std::endl;
+            free(begin);
+        }
+    }
+    void* allocate(size_t size, size_t alignment) {
+        char* aligned_address = align(offset, alignment);
+        if(aligned_address + size > end) {
+            throw std::runtime_error("not enough space");
+        }
+        char* res = aligned_address;
+        offset = aligned_address + size;
+        return res;
+    }
+    void reset();
+};
---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include "../include/arena.cpp"
+using namespace std;
+
+void test_ctor_one_par(){
+    const long long good_size = 1024;
+    arena a(good_size);
+    cout << "\n";
+    const long long bad_size = 100000000000;
+    try{
+        arena b(bad_size);
+    } catch (std::exception& e){
+        cout << e.what() << '\n';
+    }
+}
+
+void test_ctor_two_par(){
+    const int good_size = 1 << 10;
+    char buf[good_size];
+    arena a(buf, good_size);
+    try{
+        // const long long bad_size = 1e10;  works
+        const long long bad_size = 1e11; //  fails
+        // char buf[bad_size];             //stack allocation
+        {
+            char* buf2 = (char*)malloc(bad_size); // on the heap
+            arena b(buf2, bad_size);
+            free(buf2);
+        }
+    } catch (std::exception& e){
+        cout << e.what() << '\n';
+    }
+}
+
+struct Car{
+    int year;
+    string name;
+    Car() : year(2000), name("null") {};
+    ~Car() {
+        cout << "car dtor\n";
+    }
+};
+
+void car_test(){
+    arena a(1024); // empty space
+    cout << sizeof(Car) << ' ' <<  alignof(Car) << '\n';
+    void* ptr = a.allocate(sizeof(Car), alignof(Car)); // find suitable spot in owned memory
+    cout << "car at address " << ptr << '\n';
+    Car* car = (Car*)ptr;
+    new (car) Car(); // car is ptr to space inside arena, create Car at that address
+    car->~Car(); // segfault if i delete the object, because Arena owns it, not me
+}
+
+int main(){
+    return 0;
+}
