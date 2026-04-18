9bb69b9c95572c28540f31cf607eca3a71b5852f
misconceptor <thespeedcubers347@gmail.com>
2026-04-18 11:41:52 -0400

    move-semantics

--- include/arena.cpp
+++ include/arena.cpp
@@ -2,12 +2,19 @@
 #include <exception>
 #include <algorithm>
 #include <cstdlib>
-//delete copy ctor and copy =
+
+
+//destructor list
+//cross-arena copy
+
 class arena{
 private:
     char *begin, *offset, *end;
     bool owns_memory = false;
 public:
+    char* getBegin() const {
+        return begin;
+    }
     size_t size() const {
         return end - begin;
     }
@@ -23,12 +30,11 @@ public:
     size_t remaining_size() const {
         return end - offset;
     }
-    char* align(char* _offset, size_t alignment){
-        size_t addr = (size_t)_offset;
-        addr = (addr + alignment - 1) & ~(alignment - 1);
-        _offset = (char*)addr;
-        return _offset;
-    }
+
+    arena(const  arena&) = delete;
+
+    arena& operator=(const arena&) = delete;
+
     arena(size_t size){
         void* buf = malloc(size);
         if (buf) {
@@ -55,11 +61,40 @@ public:
             throw std::invalid_argument("null buffer");
         }
     }
+    arena (arena&& other) noexcept {
+        begin = other.begin;
+        end = other.end;
+        offset = other.offset;
+        owns_memory = other.owns_memory;
+        other.begin = other.end = other.offset = nullptr;
+        other.owns_memory = false;
+    }
+    arena& operator=(arena&& other) noexcept {
+        if(this == &other) return *this;
+
+        if(this->owns_memory)  this->reset();
+
+        begin = other.begin;
+        end = other.end;
+        offset = other.offset;
+        owns_memory = other.owns_memory;
+
+        other.begin = other.end = other.offset = nullptr;
+        other.owns_memory = false;
+
+        return *this;
+    }
     ~arena(){
         if(owns_memory && begin) {
-            std::cout << "dtor" << std::endl;
+            std::cout << "arena dtor" << std::endl;
             free(begin);
         }
+    }
+    char* align(char* _offset, size_t alignment){
+        size_t addr = (size_t)_offset;
+        addr = (addr + alignment - 1) & ~(alignment - 1);
+        _offset = (char*)addr;
+        return _offset;
     }
     void* allocate(size_t size, size_t alignment) {
         char* aligned_address = align(offset, alignment);
@@ -82,3 +117,7 @@ public:
         }
     }
 };
+
+
+//https://habr.com/ru/companies/otus/articles/988086/
+//https://protobuf.dev/reference/cpp/arenas/
--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,9 @@ void test_ctor_two_par(){
 struct Car{
     int year;
     string name;
-    Car() : year(2000), name("null") {};
+    Car() : year(2000), name("null") {cout << "car ctor\n";};
     ~Car() {
-        cout << "car dtor\n";
+        cout << "car destructed\n";
     }
 };
 
@@ -55,18 +55,25 @@ void car_test(){
 void test_reset() {
     const int size = 1024;
     arena a(size);
-    void* ptr = a.allocate(sizeof(Car), alignof(Car));
-    cout << "used : " << a.used() << '\n';
-    Car* car = (Car*)ptr;
-    new (car) Car();
-    a.reset();
-    cout << "used after reset : " << a.used() << '\n';
+
+    Car* car = new(a.allocate(sizeof(Car), alignof(Car))) Car();
+
+    car->~Car(); //delete car myself
+    a.reset(); // does not delete existing cars
+
+    //trying to allocate new cars to place with old cars
+    Car* car2 = new(a.allocate(sizeof(Car), alignof(Car))) Car();
+
+    cout << "cars at: ";
+    cout << car << ' ' << car2 << '\n'; // car2 is lower than car1 on 40 bytes (one car)
+    //first 40 bytes were overwritten, other bytes - abandoned
+
+    car2->~Car();
 }
 void test_release() {
     const int size = 1024;
     arena a(size);
     void* ptr = a.allocate(sizeof(Car), alignof(Car));
-    cout << "used : " << a.used() << '\n';
     Car* car = (Car*)ptr;
     cout << "ptr: " << car << '\n';
     new (car) Car();
@@ -74,8 +81,99 @@ void test_release() {
     a.release(); // now car is a dangling pointer
 }
 
-int main(){
+void nestedArenaProblem() {
+    size_t parent_size = 1024;
+    size_t child_size = 256;
+    arena parent(parent_size);
+    char* child_buf = (char*) parent.allocate(child_size, alignof(arena));
 
+    char* parent_arr = parent.getBegin() + child_size;
+    for (int i = 0; i < 100; ++i) {
+        parent_arr[i] = 5;
+    }
+
+    arena child(child_buf, parent_size); //more than parent allocated
+
+    int* corrupting_arr = (int*) child.allocate(300, alignof(int));
+
+    bool good = true;
+    for (int i = 0; i < 100; ++i) {
+        good &= parent_arr[i] == 5;
+    }
+    cout << "parent arr before allocation is " << (good ? "" : "not ") << "good\n";
+
+    for (int i = 0; i < 75; ++i) { // 300 bytes = 75 int
+        corrupting_arr[i] = 3;
+    }
+
+    for (int i = 0; i < 100; ++i) {
+        if (parent_arr[i] != 5) {
+            cout << "corruption at " << i << "\n";
+        }
+    }
+}
+
+void testMoveCtor() {
+    size_t size = 80;
+    arena a(size);
+
+    int* buf = (int*) a.allocate(20 * sizeof(int), alignof(int));
+
+    cout << "arena A before moving: \n";
+
+    cout << "size " << a.size() << ", ";
+    cout << "used " << a.used() << ", ";
+    cout << "isOwner " << a.is_owner() << "\n";
+
+    arena b = std::move(a); 
+
+    cout << "arena A after moving: \n";
+
+    cout << "size " << a.size() << ", ";
+    cout << "used " << a.used() << ", ";
+    cout << "isOwner " << a.is_owner() << "\n";
+
+    cout << "arena B after moving: \n";
+
+    cout << "size " << b.size() << ", ";
+    cout << "used " << b.used() << ", ";
+    cout << "isOwner " << b.is_owner() << "\n";
+}
+
+void testMoveOperator() {
+    size_t size = 80;
+    arena a(size);
+
+    int* buf = (int*) a.allocate(20 * sizeof(int), alignof(int));
+
+    arena b(size);
+
+    cout << "arena A before moving: \n";
+
+    cout << "size " << a.size() << ", ";
+    cout << "used " << a.used() << ", ";
+    cout << "isOwner " << a.is_owner() << "\n";
+
+    b = std::move(a); 
+
+    cout << "arena A after moving: \n";
+
+    cout << "size " << a.size() << ", ";
+    cout << "used " << a.used() << ", ";
+    cout << "isOwner " << a.is_owner() << "\n";
+
+    cout << "arena B after moving: \n";
+
+    cout << "size " << b.size() << ", ";
+    cout << "used " << b.used() << ", ";
+    cout << "isOwner " << b.is_owner() << "\n";
+    //arena does not own memory
+}
+
+
+int main(){
+    testMoveOperator();
+    //how to automatically desturct objects?
     /*
     todo
         delete = operator
